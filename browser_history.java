8e4b1979feac4a18e6058c7f9fee4569421dbe5d
Anishvl <74526223+Anishvl@users.noreply.github.com>
2024-02-25 11:19:33 +0000

    fixed error

--- browser_history.java
+++ browser_history.java
@@ -2,7 +2,7 @@ import java.util.*;
 
 public class Main {
     public static void main(String args[]) {
-        String arr[] = {"browser_history", "visit", "visit", "back",};
+        String arr[] = {"browser_history", "visit", "visit", "back"};
         String arr1[][] = {{"leetcode.com"}, {"google.com"}, {"1"}};
         String arr2[] = new String[arr.length];
         for (int i = 0; i < arr.length; i++) {
